9c1d1a20555c5de035dd75c33cd97ada0935a78c
Vladislav <vlaad360@users.noreply.github.com>
2017-06-01 18:49:57 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+/**
+ * Created by vlad on 01.06.17.
+ */
+var moment = require('moment');
+var TgFancy = require('tgfancy');
+var TELEGRAM_TOKEN = process.env.WATCHMEN_TELEGRAM_TOKEN;
+var CHAT_ID = process.env.WATCHMEN_CHAT_ID;
+
+const bot = new TgFancy(TELEGRAM_TOKEN);
+var eventHandlers = {
+
+  /**
+   * On a new outage
+   * @param {Object} service
+   * @param {Object} outage
+   * @param {Object} outage.error check error
+   * @param {number} outage.timestamp outage timestamp
+   */
+
+  onNewOutage: function (service, outage) {
+    var errorMsg = service.name + ' down!'.red + '. Error: ' + JSON.stringify(outage.error).red;
+    console.log(errorMsg);
+    bot.sendMessage(errorMsg);
+  },
+
+  /**
+   * Failed ping on an existing outage
+   * @param {Object} service
+   * @param {Object} outage
+   * @param {Object} outage.error check error
+   * @param {number} outage.timestamp outage timestamp
+   */
+
+  onCurrentOutage: function (service, outage) {
+    var errorMsg = service.name + ' is still down!'.red + '. Error: ' + JSON.stringify(outage.error).red;
+    console.log(errorMsg);
+    bot.sendMessage(errorMsg);
+
+  },
+
+  /**
+   * Failed check (it will be an outage or not according to service.failuresToBeOutage
+   * @param {Object} service
+   * @param {Object} data
+   * @param {Object} data.error check error
+   * @param {number} data.currentFailureCount number of consecutive check failures
+   */
+
+  onFailedCheck: function (service, data) {
+    var errorMsg = service.name + ' check failed!'.red + '. Error: ' + JSON.stringify(data.error).red;
+    console.log(errorMsg);
+    bot.sendMessage(errorMsg);
+
+  },
+
+  /**
+   * Warning alert
+   * @param {Object} service
+   * @param {Object} data
+   * @param {number} data.elapsedTime (ms)
+   */
+
+  onLatencyWarning: function (service, data) {
+    var msg = service.name + ' latency warning'.yellow + '. Took: ' + (data.elapsedTime + ' ms.').yellow;
+    console.log(msg);
+    bot.sendMessage(msg);
+
+  },
+
+  /**
+   * Service is back online
+   * @param {Object} service
+   * @param {Object} lastOutage
+   * @param {Object} lastOutage.error
+   * @param {number} lastOutage.timestamp (ms)
+   */
+
+  onServiceBack: function (service, lastOutage) {
+    var duration = moment.duration(+new Date() - lastOutage.timestamp, 'seconds');
+    console.log(service.name.white + ' is back'.green + '. Down for '.gray + duration.humanize().white);
+    // bot.sendMessage(errorMsg);
+
+  },
+
+  /**
+   * Service is responding correctly
+   * @param {Object} service
+   * @param {Object} data
+   * @param {number} data.elapsedTime (ms)
+   */
+
+  onServiceOk: function (service, data) {
+    var serviceOkMsg = service.name + ' responded ' + 'OK!'.green;
+    var responseTimeMsg = data.elapsedTime + ' ms.';
+    console.log(serviceOkMsg, responseTimeMsg.gray);
+
+    // bot.sendMessage(errorMsg);
+
+  }
+};
+
+function ConsolePlugin(watchmen) {
+  watchmen.on('new-outage', eventHandlers.onNewOutage);
+  watchmen.on('current-outage', eventHandlers.onCurrentOutage);
+  watchmen.on('service-error', eventHandlers.onFailedCheck);
+
+  watchmen.on('latency-warning', eventHandlers.onLatencyWarning);
+  watchmen.on('service-back', eventHandlers.onServiceBack);
+  watchmen.on('service-ok', eventHandlers.onServiceOk);
+}
+
+exports = module.exports = ConsolePlugin;
