79d439881c9345d1a1eb9bdb24007fb98185b441
vlaad360 <vlad3600@gmail.com>
2017-06-02 23:21:43 +0300

    Refactoring and fix issue with wrong duration calculation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # watchman-plugin-telegram
+Plugin that allow watchman to send messages through Telegram Bot.
+
+**WATCHMEN_TELEGRAM_TOKEN** is the bot token \
+**WATCHMEN_CHAT_ID** is the chat id
--- index.js
+++ index.js
@@ -3,109 +3,83 @@
  */
 var moment = require('moment');
 var TgFancy = require('tgfancy');
+
 var TELEGRAM_TOKEN = process.env.WATCHMEN_TELEGRAM_TOKEN;
 var CHAT_ID = process.env.WATCHMEN_CHAT_ID;
 
 const bot = new TgFancy(TELEGRAM_TOKEN);
 var eventHandlers = {
 
-  /**
-   * On a new outage
-   * @param {Object} service
-   * @param {Object} outage
-   * @param {Object} outage.error check error
-   * @param {number} outage.timestamp outage timestamp
-   */
+    /**
+     * On a new outage
+     * @param {Object} service
+     * @param {Object} outage
+     * @param {Object} outage.error check error
+     * @param {number} outage.timestamp outage timestamp
+     */
+    onNewOutage: function (service, outage) {
+        var errorMsg = service.name + ' down!' + '. Error: ' + JSON.stringify(outage.error);
+        bot.sendMessage(CHAT_ID, errorMsg);
+    },
 
-  onNewOutage: function (service, outage) {
-    var errorMsg = service.name + ' down!'.red + '. Error: ' + JSON.stringify(outage.error).red;
-    console.log(errorMsg);
-    bot.sendMessage(CHAT_ID, errorMsg);
-  },
+    /**
+     * Failed ping on an existing outage
+     * @param {Object} service
+     * @param {Object} outage
+     * @param {Object} outage.error check error
+     * @param {number} outage.timestamp outage timestamp
+     */
+    onCurrentOutage: function (service, outage) {
+        var errorMsg = service.name + ' is still down!' + '. Error: ' + JSON.stringify(outage.error);
+        bot.sendMessage(CHAT_ID, errorMsg);
+    },
 
-  /**
-   * Failed ping on an existing outage
-   * @param {Object} service
-   * @param {Object} outage
-   * @param {Object} outage.error check error
-   * @param {number} outage.timestamp outage timestamp
-   */
+    /**
+     * Failed check (it will be an outage or not according to service.failuresToBeOutage
+     * @param {Object} service
+     * @param {Object} data
+     * @param {Object} data.error check error
+     * @param {number} data.currentFailureCount number of consecutive check failures
+     */
+    onFailedCheck: function (service, data) {
+        var errorMsg = service.name + ' check failed!' + '. Error: ' + JSON.stringify(data.error);
+        bot.sendMessage(CHAT_ID, errorMsg);
+    },
 
-  onCurrentOutage: function (service, outage) {
-    var errorMsg = service.name + ' is still down!'.red + '. Error: ' + JSON.stringify(outage.error).red;
-    console.log(errorMsg);
-    bot.sendMessage(CHAT_ID, errorMsg);
+    /**
+     * Warning alert
+     * @param {Object} service
+     * @param {Object} data
+     * @param {number} data.elapsedTime (ms)
+     */
 
-  },
+    onLatencyWarning: function (service, data) {
+        var msg = service.name + ' latency warning' + '. Took: ' + (data.elapsedTime + ' ms.');
+        bot.sendMessage(CHAT_ID, msg);
+    },
 
-  /**
-   * Failed check (it will be an outage or not according to service.failuresToBeOutage
-   * @param {Object} service
-   * @param {Object} data
-   * @param {Object} data.error check error
-   * @param {number} data.currentFailureCount number of consecutive check failures
-   */
+    /**
+     * Service is back online
+     * @param {Object} service
+     * @param {Object} lastOutage
+     * @param {Object} lastOutage.error
+     * @param {number} lastOutage.timestamp (ms)
+     */
 
-  onFailedCheck: function (service, data) {
-    var errorMsg = service.name + ' check failed!'.red + '. Error: ' + JSON.stringify(data.error).red;
-    console.log(errorMsg);
-    bot.sendMessage(CHAT_ID, errorMsg);
-
-  },
-
-  /**
-   * Warning alert
-   * @param {Object} service
-   * @param {Object} data
-   * @param {number} data.elapsedTime (ms)
-   */
-
-  onLatencyWarning: function (service, data) {
-    var msg = service.name + ' latency warning'.yellow + '. Took: ' + (data.elapsedTime + ' ms.').yellow;
-    console.log(msg);
-    bot.sendMessage(CHAT_ID, msg);
-
-  },
-
-  /**
-   * Service is back online
-   * @param {Object} service
-   * @param {Object} lastOutage
-   * @param {Object} lastOutage.error
-   * @param {number} lastOutage.timestamp (ms)
-   */
-
-  onServiceBack: function (service, lastOutage) {
-    var duration = moment.duration(+new Date() - lastOutage.timestamp, 'seconds');
-    var msg = service.name.white + ' is back'.green + '. Down for '.gray + duration.humanize().white;
-    bot.sendMessage(CHAT_ID, msg);
-  },
-
-  /**
-   * Service is responding correctly
-   * @param {Object} service
-   * @param {Object} data
-   * @param {number} data.elapsedTime (ms)
-   */
-
-  onServiceOk: function (service, data) {
-    var serviceOkMsg = service.name + ' responded ' + 'OK!'.green;
-    var responseTimeMsg = data.elapsedTime + ' ms.';
-    console.log(serviceOkMsg, responseTimeMsg.gray);
-
-    // bot.sendMessage(errorMsg);
-
-  }
+    onServiceBack: function (service, lastOutage) {
+        var duration = moment.duration(moment().unix() - lastOutage.timestamp, 'seconds');
+        var msg = service.name + ' is back' + '. Down for ' + duration.humanize();
+        bot.sendMessage(CHAT_ID, msg);
+    }
 };
 
-function ConsolePlugin(watchmen) {
-  watchmen.on('new-outage', eventHandlers.onNewOutage);
-  watchmen.on('current-outage', eventHandlers.onCurrentOutage);
-  watchmen.on('service-error', eventHandlers.onFailedCheck);
+function TelegramPlugin(watchmen) {
+    watchmen.on('new-outage', eventHandlers.onNewOutage);
+    watchmen.on('current-outage', eventHandlers.onCurrentOutage);
+    watchmen.on('service-error', eventHandlers.onFailedCheck);
 
-  watchmen.on('latency-warning', eventHandlers.onLatencyWarning);
-  watchmen.on('service-back', eventHandlers.onServiceBack);
-  watchmen.on('service-ok', eventHandlers.onServiceOk);
+    watchmen.on('latency-warning', eventHandlers.onLatencyWarning);
+    watchmen.on('service-back', eventHandlers.onServiceBack);
 }
 
-exports = module.exports = ConsolePlugin;
+exports = module.exports = TelegramPlugin;
